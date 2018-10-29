cd60bf3a39b7393e734eb34cd91479c721e33038
Enoch Chu <git@enochchu.com>
2018-10-29 00:45:07 -0700

    react-frontend - create add functionality

--- react-frontend/src/component/ListOfNodes.js
+++ react-frontend/src/component/ListOfNodes.js
@@ -9,12 +9,35 @@ class ListOfNodes extends Component {
             nodes: []
         }
 
+        this._handleAdd = this._handleAdd.bind(this);
         this._handleClick = this._handleClick.bind(this);
         this._refreshData = this._refreshData.bind(this);
     }
 
     componentDidMount() {
         this._refreshData();
+    }
+
+    _handleAdd(event) {
+        let container = event.target.closest("div");
+        let input = container.querySelector("input");
+        let inputValue = input.value;
+
+        if (inputValue.value === "") {
+            return;
+        }
+
+        fetch('http://localhost:8080/nodes/new', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": inputValue
+            })
+        }).then(() => this._refreshData());
+
+        input.value = "";
     }
 
     _handleClick(event) {
@@ -65,6 +88,11 @@ class ListOfNodes extends Component {
                         ))
                     }
                 </ul>
+
+                <div className="add">
+                    <input />
+                    <button onClick={ this._handleAdd }>Add</button>
+                </div>
             </div>
         );
     }
